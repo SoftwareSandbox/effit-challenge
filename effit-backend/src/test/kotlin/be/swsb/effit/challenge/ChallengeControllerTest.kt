0f70e903749367125e72d59f81191a1e4c808d37
sch3lp <sch3lp@gmail.com>
2019-04-06 18:18:07 +0200

    Desperate attempt at fixing controllertests interdependency to repotest

--- effit-backend/src/test/kotlin/be/swsb/effit/challenge/ChallengeControllerTest.kt
+++ effit-backend/src/test/kotlin/be/swsb/effit/challenge/ChallengeControllerTest.kt
@@ -6,7 +6,7 @@ import io.kotlintest.spring.SpringListener
 import io.mockk.every
 import io.mockk.mockk
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
@@ -16,8 +16,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@WebMvcTest(controllers = [ChallengeController::class])
-@AutoConfigureMockMvc
+@WebMvcTest(controllers = [ChallengeController::class],
+            excludeAutoConfiguration = [HibernateJpaAutoConfiguration::class])
 class ChallengeControllerTest : StringSpec() {
     override fun listeners() = listOf(SpringListener)
 
@@ -25,7 +25,7 @@ class ChallengeControllerTest : StringSpec() {
     lateinit var mockMvc: MockMvc
 
     @Autowired
-    lateinit var challengeRepository: ChallengeRepository
+    lateinit var challengeRepositoryMock: ChallengeRepository
 
     @TestConfiguration
     class ChallengeControllerTestConfig {
@@ -37,7 +37,7 @@ class ChallengeControllerTest : StringSpec() {
         "GET /api/challenge should return all Challenges" {
             val expectedChallenges = listOf(Challenge(name = "Playboy", points = 7, description = "ride down a slope with exposed torso"))
 
-            every { challengeRepository.findAll() } returns expectedChallenges
+            every { challengeRepositoryMock.findAll() } returns expectedChallenges
 
             mockMvc.perform(MockMvcRequestBuilders.get("/api/challenge")
                     .accept(MediaType.APPLICATION_JSON_UTF8))
