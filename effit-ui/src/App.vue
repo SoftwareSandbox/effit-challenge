a8144c862d18c8dfca0537ec1e88a49b21940cc0
sch3lp <sch3lp@gmail.com>
2019-04-07 15:27:07 +0200

    Introduced router-view

--- effit-ui/src/App.vue
+++ effit-ui/src/App.vue
@@ -31,27 +31,7 @@
     </v-toolbar>
     <v-content>
       <v-container fluid fill-height>
-        <!-- To be replaced with router-outlet  -->
-        <v-layout justify-center align-center>
-          <v-flex shrink>
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn :href="source" icon large target="_blank" v-on="on">
-                  <v-icon large>code</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn icon large href="https://codepen.io/johnjleider/pen/qxQWda" target="_blank" v-on="on">
-                  <v-icon large>mdi-codepen</v-icon>
-                </v-btn>
-              </template>
-              <span>Codepen</span>
-            </v-tooltip>
-          </v-flex>
-        </v-layout>
+        <router-view></router-view>
       </v-container>
     </v-content>
     <v-footer app fixed>
