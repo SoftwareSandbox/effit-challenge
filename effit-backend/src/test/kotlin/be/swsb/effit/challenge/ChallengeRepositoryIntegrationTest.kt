5d94a83b1b4ae3be2c7a6c5a6aea22592fd762b6
sch3lp <sch3lp@gmail.com>
2019-04-07 14:02:50 +0200

    Using TestEntityManager to persist instead of repo.save()

--- effit-backend/src/test/kotlin/be/swsb/effit/challenge/ChallengeRepositoryIntegrationTest.kt
+++ effit-backend/src/test/kotlin/be/swsb/effit/challenge/ChallengeRepositoryIntegrationTest.kt
@@ -4,12 +4,18 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @DataJpaTest
+@AutoConfigureTestEntityManager
 class ChallengeRepositoryIntegrationTest {
+
+    @Autowired
+    lateinit var testEntityManager: TestEntityManager
 
     @Autowired
     lateinit var challengeRepository: ChallengeRepository
@@ -18,9 +24,8 @@ class ChallengeRepositoryIntegrationTest {
     fun `findAll should return all of the Challenges that are persisted in the DB`() {
         val playboyChallenge = Challenge(name = "Playboy", points = 7, description = "playboy description")
         val picassoChallenge = Challenge(name = "Picasso", points = 3, description = "picasso description")
-
-        challengeRepository.save(playboyChallenge)
-        challengeRepository.save(picassoChallenge)
+        testEntityManager.persist(playboyChallenge)
+        testEntityManager.persist(picassoChallenge)
 
         val actual = challengeRepository.findAll()
 
