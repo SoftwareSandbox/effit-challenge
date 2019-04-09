080d27d35e656405b6460822f708a3b998409811
sch3lp <sch3lp@gmail.com>
2019-04-09 14:28:18 +0200

    Fixed JUnit4 to JUnit5 migration (was still using @Test from JUnit4)

--- effit-backend/src/test/kotlin/be/swsb/effit/challenge/ChallengeRepositoryIntegrationTest.kt
+++ effit-backend/src/test/kotlin/be/swsb/effit/challenge/ChallengeRepositoryIntegrationTest.kt
@@ -1,7 +1,7 @@
 package be.swsb.effit.challenge
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Test
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager
