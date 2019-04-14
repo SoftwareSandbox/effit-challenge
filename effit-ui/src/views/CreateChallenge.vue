0a32068c1c34c0bd5e85863f362f0a2fa5e4d73b
sch3lp <sch3lp@gmail.com>
2019-04-14 15:27:00 +0200

    Show notification upon successful submit. Fixes #14

--- effit-ui/src/views/CreateChallenge.vue
+++ effit-ui/src/views/CreateChallenge.vue
@@ -23,6 +23,16 @@
             ></v-textarea>
             <v-btn @click="submit">submit</v-btn>
         </v-form>
+
+        <v-snackbar
+                v-model="showSnackbar"
+                top
+                color="cyan darken-2"
+                :timeout="snackbarTimeout"
+        >
+            {{ snackbarMessage }}
+            <v-btn dark flat @click="closeSnackbar()">Close</v-btn>
+        </v-snackbar>
     </v-layout>
 </template>
 
@@ -36,15 +46,28 @@
     export default class CreateChallenge extends Vue {
         protected challenge = {name: '', points: 0, description: ''};
         protected successfullyCreatedChallengeId: string = '';
+        // snackbar stuff
+        protected showSnackbar = false;
+        protected snackbarMessage = '';
+        protected snackbarTimeout = 3000;
 
         private submit() {
             this.$axios.post(`/api/challenge`, this.challenge)
-                .then((res) => this.successfullyCreatedChallengeId = res.headers.location)
+                .then((res) => {
+                    this.successfullyCreatedChallengeId = res.headers.location;
+                    this.snackbarMessage = `Successfully created your new Challenge!`;
+                    this.showSnackbar = true;
+                })
                 .then(() => this.resetForm());
         }
 
         private resetForm() {
-            return this.challenge = {name: '', points: 0, description: ''};
+            this.challenge = {name: '', points: 0, description: ''};
+        }
+
+        private closeSnackbar() {
+            this.showSnackbar = false;
+            this.snackbarMessage = '';
         }
     }
 </script>
