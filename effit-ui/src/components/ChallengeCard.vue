3fc781ce027fa8a506cc33fdf3e19ee4bc44bc92
sch3lp <sch3lp@gmail.com>
2019-04-20 15:36:33 +0200

    Introduced ChallengeCard component. Thanks Jared!

---
+++ effit-ui/src/components/ChallengeCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-card>
+        <v-layout row justify-space-between>
+            <v-card-title><h3>{{challenge.name}}</h3></v-card-title>
+            <div><v-chip title="Points">{{challenge.points}}</v-chip></div>
+        </v-layout>
+        <v-card-text>{{challenge.description}}</v-card-text>
+    </v-card>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+
+    @Component({
+        components: {},
+    })
+    export default class ChallengeCard extends Vue {
+        @Prop({type: Array}) public challenge !: { name: string, points: number, description: string };
+
+
+    }
+</script>
