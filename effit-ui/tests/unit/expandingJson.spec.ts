e62291477e94336eabf9ca6f36b81cb9fa53ed9a
sch3lp <sch3lp@gmail.com>
2019-05-05 14:22:54 +0200

    Fixed lint errors

--- effit-ui/tests/unit/expandingJson.spec.ts
+++ effit-ui/tests/unit/expandingJson.spec.ts
@@ -2,14 +2,14 @@
 
 describe('expanding incoming json data', () => {
     it('adding a selectable property', () => {
-        type SelectableData = {name:string, selected:boolean};
+        type SelectableData = {name: string, selected: boolean};
 
-        let data: any[] = [{name:"Snarf"}, {name:"Lion-O"}];
+        let data: any[] = [{name: 'Snarf'}, {name: 'Lion-O'}];
 
-        let expandedData = data.map((challengeFromData) => {
+        const expandedData = data.map((challengeFromData) => {
             return {...challengeFromData, ...{selected: false}};
         });
 
-        expect(expandedData).toEqual([{name: "Snarf", selected: false}, {name: "Lion-O", selected: false}]);
+        expect(expandedData).toEqual([{name: 'Snarf', selected: false}, {name: 'Lion-O', selected: false}]);
     });
 });
