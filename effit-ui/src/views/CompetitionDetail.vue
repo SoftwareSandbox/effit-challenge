470a856c1f12af7789f05dc4a259f528f2440ad6
sch3lp <sch3lp@gmail.com>
2019-05-19 17:10:28 +0200

    Added competitor name to the competition detail page

--- effit-ui/src/views/CompetitionDetail.vue
+++ effit-ui/src/views/CompetitionDetail.vue
@@ -5,6 +5,16 @@
                 :challenges="competition.challenges"
                 :rowHandler="navigateToMarkAsCompleted">
         </challenges-table>
+        <v-form @submit.prevent="addCompetitor">
+            <v-text-field
+                    v-model="competitorName"
+                    :counter="50"
+                    label="Competitor to add"
+                    required
+            ></v-text-field>
+
+            <v-btn type="submit">add competitor</v-btn>
+        </v-form>
     </v-layout>
 </template>
 
@@ -12,6 +22,7 @@
     import {Component, Prop, Vue} from 'vue-property-decorator';
     import ChallengesTable from '@/components/ChallengesTable.vue';
     import {Challenge} from '@/model/Challenge';
+    import {noop} from 'vue-class-component/lib/util';
 
     @Component({
         components: {ChallengesTable},
@@ -19,6 +30,7 @@
     export default class CompetitionDetail extends Vue {
         @Prop({type: String}) protected competitionId!: string;
         protected competition = {name: '', startDate: '', endDate: ''};
+        protected competitorName : string = '';
 
         private mounted() {
             this.$axios.get(`/api/competition/${this.competitionId}`)
@@ -28,6 +40,11 @@
 
         private navigateToMarkAsCompleted(challenge: Challenge) {
             this.$router.push(`/competitions/${this.competitionId}/complete/${challenge.id}`);
+        }
+
+        private addCompetitor() {
+            this.$axios.post(`/api/competition/${this.competitionId}/addCompetitor`, {name: this.competitorName})
+                .catch(noop);
         }
     }
 </script>
