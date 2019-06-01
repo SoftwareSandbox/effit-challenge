c4e9459c536c18cf8e9a4a94d5d6feb04d394ef4
sch3lp <sch3lp@gmail.com>
2019-06-01 13:56:43 +0200

    Introduced completed Challenges to a Competitor

--- effit-backend/src/main/kotlin/be/swsb/effit/competition/Competitor.kt
+++ effit-backend/src/main/kotlin/be/swsb/effit/competition/Competitor.kt
@@ -1,14 +1,26 @@
 package be.swsb.effit.competition
 
+import be.swsb.effit.challenge.Challenge
 import be.swsb.effit.util.RestApiExposed
 import java.util.*
 import javax.persistence.Entity
 import javax.persistence.Id
+import javax.persistence.ManyToMany
 
 @Entity
 data class Competitor(@Id val id: UUID = UUID.randomUUID(),
                       val name: String,
                       var totalScore: Int): RestApiExposed {
+
+    @ManyToMany
+    private var _completedChallenges: List<Challenge> = emptyList()
+
+    val completedChallenges: List<Challenge>
+        get() = _completedChallenges
+
+    fun completeChallenge(challenge: Challenge) {
+        _completedChallenges = _completedChallenges + challenge
+    }
 
     fun awardPoints(points: Int) {
         totalScore += points
--- effit-backend/src/test/kotlin/be/swsb/effit/competition/competitor/CompetitorRepositoryIntegrationTest.kt
+++ effit-backend/src/test/kotlin/be/swsb/effit/competition/competitor/CompetitorRepositoryIntegrationTest.kt
@@ -1,5 +1,6 @@
 package be.swsb.effit.competition.competitor
 
+import be.swsb.effit.challenge.Challenge
 import be.swsb.effit.competition.Competitor
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -29,5 +30,28 @@ class CompetitorRepositoryIntegrationTest {
         testEntityManager.clear()
 
         assertThat(testEntityManager.find(Competitor::class.java, snarf.id)).isEqualTo(snarf)
+    }
+
+    @Test
+    fun `A Competitor can have completed Challenges`() {
+        val challenge = Challenge(name = "whinge", points = 4, description = "whatevs")
+        testEntityManager.persistAndFlush(challenge)
+        val anotherChallenge = Challenge(name = "whinge", points = 4, description = "different challenge")
+        testEntityManager.persistAndFlush(anotherChallenge)
+
+        val snarf = Competitor(name = "Snarf", totalScore = 6)
+        competitorRepository.save(snarf)
+        testEntityManager.flush()
+        testEntityManager.clear()
+
+        snarf.completeChallenge(challenge)
+        competitorRepository.save(snarf)
+        testEntityManager.flush()
+        testEntityManager.clear()
+
+        val fetchedSnarf = testEntityManager.find(Competitor::class.java, snarf.id)
+        assertThat(fetchedSnarf.completedChallenges)
+                .containsExactly(challenge)
+                .doesNotContain(anotherChallenge)
     }
 }
