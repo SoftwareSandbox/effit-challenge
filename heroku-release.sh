e1f8ba10759617478ee9b18c7b88d36248e016d4
sch3lp <sch3lp@gmail.com>
2019-07-15 19:53:17 +0200

    try out with basic auth to heroku api

--- heroku-release.sh
+++ heroku-release.sh
@@ -1,6 +1,7 @@
 
 export WEB_DOCKER_IMAGE_ID="$(docker inspect swsb/effitchallenge --format={{.Id}})"
 curl -n -X PATCH https://api.heroku.com/apps/effit-challenge/formation \
+     --user $HEROKU_USERNAME:$HEROKU_AUTH_TOKEN
      -H "Content-Type: application/json" \
      -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
      -d @- <<EOF
