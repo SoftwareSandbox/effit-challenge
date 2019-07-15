b16ca9ab474a5081c9bff647af8159d79f5e6c2b
sch3lp <sch3lp@gmail.com>
2019-07-15 19:32:59 +0200

    releasing using heroku API :pray:

--- heroku-release.sh
+++ heroku-release.sh
@@ -1,12 +1,15 @@
 
+export WEB_DOCKER_IMAGE_ID="$(docker inspect swsb/effitchallenge --format={{.Id}})"
 curl -n -X PATCH https://api.heroku.com/apps/effit-challenge/formation \
-  -d '{
+     -H "Content-Type: application/json" \
+     -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
+     -d @- <<EOF
+{
   "updates": [
     {
       "type": "web",
       "docker_image": "$WEB_DOCKER_IMAGE_ID"
-    },
+    }
   ]
-}' \
-  -H "Content-Type: application/json" \
-  -H "Accept: application/vnd.heroku+json; version=3.docker-releases"
+}
+EOF
