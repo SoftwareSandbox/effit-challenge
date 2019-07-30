d64234d6366eb53ff429673ae3e1a62eda3b503c
sch3lp <sch3lp@gmail.com>
2019-07-30 15:38:24 +0200

    Fixed breaking test on TravisCI

--- effit-backend/src/test/kotlin/be/swsb/effit/frameworktests/JacksonObjectMapperTest.kt
+++ effit-backend/src/test/kotlin/be/swsb/effit/frameworktests/JacksonObjectMapperTest.kt
@@ -10,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(classes = [EffitApplication::class])
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class JacksonObjectMapperTest {
 
     @Autowired
