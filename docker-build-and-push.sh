232b69b5dd24ecd55ef2cf9aee6bd6f38dddd74e
sch3lp <sch3lp@gmail.com>
2019-08-21 20:59:36 +0200

    docker log in from the yml, hopefully retained

--- docker-build-and-push.sh
+++ docker-build-and-push.sh
@@ -1,4 +1,3 @@
-echo "${{ secrets.HEROKU_AUTH_TOKEN }}" | docker login --username="${{ secrets.HEROKU_USERNAME }}" --password-stdin registry.heroku.com;
 docker build ./ops/webapp -t swsb/effitchallenge;
 docker tag swsb/effitchallenge registry.heroku.com/effit-challenge/web;
 docker push registry.heroku.com/effit-challenge/web;
