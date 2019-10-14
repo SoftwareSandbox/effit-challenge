e3b2fb87b3f93fb44feb11698c184bfdb306b0eb
sch3lp <sch3lp@gmail.com>
2019-10-14 18:55:11 +0200

    Updated deployment to use cqrs-webapp

--- ops/build.gradle.kts
+++ ops/build.gradle.kts
@@ -2,9 +2,9 @@ tasks.register("copyWebApplicationJar", Copy::class) {
     group = "build"
     description = "Copy web application jar to be picked up by Docker"
 
-    dependsOn(":effit-backend:build")
+    dependsOn(":effit-cqrs-backend:build")
 
-    from("../effit-backend/build/libs/effit-webapp.jar")
+    from("../effit-cqrs-backend/build/libs/effit-cqrs-webapp.jar")
     into("webapp")
 }
 
