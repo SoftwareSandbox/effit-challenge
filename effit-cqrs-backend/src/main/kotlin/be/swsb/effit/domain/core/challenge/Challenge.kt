8906e93e98a18d8f859ac459c47c38965fa5a72e
sch3lp <sch3lp@gmail.com>
2019-11-03 16:37:08 +0100

    Changed Challenge's IllegalStateException to DomainRuntimeValidations

--- effit-cqrs-backend/src/main/kotlin/be/swsb/effit/domain/core/challenge/Challenge.kt
+++ effit-cqrs-backend/src/main/kotlin/be/swsb/effit/domain/core/challenge/Challenge.kt
@@ -1,6 +1,7 @@
 package be.swsb.effit.domain.core.challenge
 
 import be.swsb.effit.adapter.ui.util.RestApiExposed
+import be.swsb.effit.domain.core.exceptions.DomainValidationRuntimeException
 import java.util.*
 import javax.persistence.Entity
 import javax.persistence.Id
@@ -12,8 +13,8 @@ data class Challenge(@Id val id: UUID = UUID.randomUUID(),
                      val description: String) : RestApiExposed {
 
     init {
-        if (points < 0) throw IllegalStateException("Cannot create a Challenge with negative points")
-        if (points == 0) throw IllegalStateException("Cannot create a Challenge with 0 points")
+        if (points < 0) throw DomainValidationRuntimeException("Cannot create a Challenge with negative points")
+        if (points == 0) throw DomainValidationRuntimeException("Cannot create a Challenge with 0 points")
     }
 
     companion object
--- effit-cqrs-backend/src/test/kotlin/be/swsb/effit/domain/core/challenge/ChallengeTest.kt
+++ effit-cqrs-backend/src/test/kotlin/be/swsb/effit/domain/core/challenge/ChallengeTest.kt
@@ -1,24 +1,24 @@
 package be.swsb.effit.domain.core.challenge
 
-import be.swsb.effit.domain.core.challenge.Challenge
-import org.assertj.core.api.Assertions
+import be.swsb.effit.domain.core.exceptions.DomainValidationRuntimeException
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
 class ChallengeTest {
 
     @Test
     fun `should fail with negative points`() {
-        Assertions.assertThatThrownBy {
+        assertThatThrownBy {
             Challenge(name = "Playboy", points = -7, description = "ride down a slope with exposed torso")
-        }.isInstanceOf(IllegalStateException::class.java)
+        }.isInstanceOf(DomainValidationRuntimeException::class.java)
         .hasMessage("Cannot create a Challenge with negative points")
     }
 
     @Test
     fun `should fail with 0 points`() {
-        Assertions.assertThatThrownBy {
+        assertThatThrownBy {
             Challenge(name = "Playboy", points = 0, description = "ride down a slope with exposed torso")
-        }.isInstanceOf(IllegalStateException::class.java)
+        }.isInstanceOf(DomainValidationRuntimeException::class.java)
                 .hasMessage("Cannot create a Challenge with 0 points")
     }
 }
