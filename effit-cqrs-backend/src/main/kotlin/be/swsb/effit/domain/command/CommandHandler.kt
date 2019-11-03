be19684c9e8deac6a9b397f6b155b663bf798853
sch3lp <sch3lp@gmail.com>
2019-11-03 16:36:08 +0100

    Renamed Q (for Query) to C (for Command)

--- effit-cqrs-backend/src/main/kotlin/be/swsb/effit/domain/command/CommandHandler.kt
+++ effit-cqrs-backend/src/main/kotlin/be/swsb/effit/domain/command/CommandHandler.kt
@@ -1,6 +1,6 @@
 package be.swsb.effit.domain.command
 
-interface CommandHandler<out R, Q : Command<out R>> {
-    fun handle(command: Q): R
-    fun getCommandType(): Class<Q>
+interface CommandHandler<out R, C : Command<out R>> {
+    fun handle(command: C): R
+    fun getCommandType(): Class<C>
 }
