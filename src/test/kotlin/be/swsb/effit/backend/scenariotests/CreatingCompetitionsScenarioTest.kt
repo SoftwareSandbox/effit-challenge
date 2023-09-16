d155e207631518f75c0443bf161f0acc6ff0fec7
Sch3lp <sch3lp@gmail.com>
2023-09-17 00:47:54 +0200

    Detected some unwanted behaviour in our scenariotests

--- src/test/kotlin/be/swsb/effit/backend/scenariotests/CreatingCompetitionsScenarioTest.kt
+++ src/test/kotlin/be/swsb/effit/backend/scenariotests/CreatingCompetitionsScenarioTest.kt
@@ -4,12 +4,9 @@ import be.swsb.effit.backend.domain.command.challenge.UpdateChallengeCommandHand
 import be.swsb.effit.backend.domain.command.competition.*
 import be.swsb.effit.backend.domain.command.competition.competitor.AddCompetitorCommandHandler
 import be.swsb.effit.backend.domain.command.competition.competitor.RemoveCompetitorCommandHandler
-import be.swsb.effit.backend.domain.core.challenge.Challenge
 import be.swsb.effit.backend.domain.core.challenge.ChallengeRepository
-import be.swsb.effit.backend.domain.core.competition.Competition
 import be.swsb.effit.backend.domain.core.competition.CompetitionAlreadyExistsDomainException
 import be.swsb.effit.backend.domain.core.competition.CompetitionCreator
-import be.swsb.effit.backend.domain.core.competition.CompetitionId
 import be.swsb.effit.backend.domain.core.exceptions.DomainValidationRuntimeException
 import be.swsb.effit.backend.domain.query.challenge.FindChallengeQueryHandler
 import be.swsb.effit.backend.domain.query.competition.FindAllCompetitionsQueryHandler
@@ -18,9 +15,7 @@ import be.swsb.effit.backend.domain.query.competition.MaybeFindCompetitionQueryH
 import be.swsb.effit.backend.messaging.command.CommandExecutor
 import be.swsb.effit.backend.messaging.query.QueryExecutor
 import java.time.LocalDate
-import java.util.*
-import kotlin.test.Test
-import kotlin.test.assertFailsWith
+import kotlin.test.*
 
 class CreatingCompetitionsScenarioTest {
     private val competitionRepository: CompetitionRepository = InMemCompetitionRepository()
@@ -62,102 +57,138 @@ class CreatingCompetitionsScenarioTest {
 
     @Test
     fun `Competition that ends up having an already existing CompetitionId should not be created`() {
-        val competition = CreateCompetition(name = "Thundercats%Competition",
-                startDate = LocalDate.of(2018, 3, 16),
-                endDate = LocalDate.of(2018, 3, 26))
+        val competition = CreateCompetition(
+            name = "Thundercats%Competition",
+            startDate = LocalDate.of(2018, 3, 16),
+            endDate = LocalDate.of(2018, 3, 26)
+        )
         scenarios.createNewCompetition(competition)
 
-        val competitionWithIdThatAlreadyExists = CreateCompetition(name = "Thundercats Competition",
-                startDate = LocalDate.of(2019, 1, 17),
-                endDate = LocalDate.of(2019, 1, 27))
+        val competitionWithIdThatAlreadyExists = CreateCompetition(
+            name = "Thundercats Competition",
+            startDate = LocalDate.of(2019, 1, 17),
+            endDate = LocalDate.of(2019, 1, 27)
+        )
 
         assertFailsWith(CompetitionAlreadyExistsDomainException::class) {
             scenarios.createNewCompetition(competitionWithIdThatAlreadyExists)
         }
     }
-//
-//    @Test
-//    fun `Adding Challenges, one of which is invalid, should not add any Challenge`() {
-//        val challengeToBeCreated = ChallengeToAdd(name = "Picasso", points = 3, description = "Paint a mustache on a sleeping victim without getting caught")
-//        val anotherChallengeToBeCreated = ChallengeToAdd(name = "SomethingElse", points = 5, description = "Something...")
-//        val invalidChallengeToBeCreated = ChallengeToAdd(name = "Invalid Challenge", points = 0, description = "Invalid because points cannot be 0")
-//
-//        val competition = CreateCompetition(name = "InvalidChallengesComp",
-//                startDate = LocalDate.of(2018, 3, 16),
-//                endDate = LocalDate.of(2018, 3, 26))
-//        val competitionId = scenarios.createNewCompetition(competition)
-//        assertThat(scenarios.getCompetition(competitionId).challenges).isEmpty()
-//
-//        val challengesToBeAdded = listOf(challengeToBeCreated, invalidChallengeToBeCreated, anotherChallengeToBeCreated)
-//        scenarios.mockMvc.perform(MockMvcRequestBuilders.post("/api/competition/{id}/addChallenges", competitionId.id)
-//                .content(challengesToBeAdded.toJson(scenarios.objectMapper))
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(MockMvcResultMatchers.status().isBadRequest)
-//
-//        assertThat(scenarios.getCompetition(competitionId).challenges).isEmpty()
-//    }
-//
-//    @Test
-//    fun `Challenges should get copied when hosting a Competition again`() {
-//        val challengeToBeCreated = ChallengeToAdd(name = "Picasso", points = 3, description = "Paint a mustache on a sleeping victim without getting caught")
-//
-//        val competition = CreateCompetition(name = "DummyCompetition",
-//                startDate = LocalDate.of(2018, 3, 16),
-//                endDate = LocalDate.of(2018, 3, 26))
-//        val competitionId = scenarios.createNewCompetition(competition, challengeToBeCreated)
-//        val originalChallengeId = scenarios.getCompetition(competitionId).challenges.first().id
-//
-//        val hostedAgainCompetition = CreateCompetition(name = "DummyCompetitionThe2nd",
-//                startDate = LocalDate.of(2018, 3, 16),
-//                endDate = LocalDate.of(2018, 3, 26))
-//        val hostedAgainCompetitionId = scenarios.createNewCompetition(hostedAgainCompetition, challengeToBeCreated)
-//
-//        assertThat(scenarios.getCompetition(hostedAgainCompetitionId).challenges.first().id)
-//                .isNotEqualTo(originalChallengeId)
-//    }
-//
-//    @Test
-//    fun `Challenges can get updated when a Competition is not yet started`() {
-//        val challengeToBeCreated = ChallengeToAdd(name = "Picasso", points = 3, description = "Paint a mustache on a sleeping victim without getting caught")
-//
-//        val competition = CreateCompetition(name = "CompetitionWChallenges",
-//                startDate = LocalDate.of(2018, 3, 16),
-//                endDate = LocalDate.of(2018, 3, 26))
-//        val competitionId = scenarios.createNewCompetition(competition, challengeToBeCreated)
-//
-//        val challengeToUpdate = scenarios.getCompetition(competitionId).challenges.find { it.name == challengeToBeCreated.name }
-//                ?: fail("Expected a challenge with name ${challengeToBeCreated.name}")
-//
-//        scenarios.updateChallenge(challengeToUpdate.copy(name = "Francisco"))
-//
-//        assertThat(scenarios.getCompetition(competitionId).challenges)
-//                .extracting<String> { it.name }
-//                .containsOnly("Francisco")
-//    }
-//
-//    @Test
-//    fun `Challenges can get updated when a Competition was unstarted`() {
-//        val challengeToBeCreated = ChallengeToAdd(name = "Picasso", points = 3, description = "Paint a mustache on a sleeping victim without getting caught")
-//
-//        val competition = CreateCompetition(name = "UnstartedCompWChallenges",
-//                startDate = LocalDate.of(2018, 3, 16),
-//                endDate = LocalDate.of(2018, 3, 26))
-//        val competitionId = scenarios.createNewCompetition(competition, challengeToBeCreated)
-//        scenarios.addCompetitor("Snarf", competitionId)
-//
-//        scenarios.startCompetition(competitionId)
-//        scenarios.unstartCompetition(competitionId)
-//
-//        val challengeToUpdate = scenarios.getCompetition(competitionId).challenges.find { it.name == challengeToBeCreated.name }
-//                ?: fail("Expected a challenge with name ${challengeToBeCreated.name}")
-//
-//        scenarios.updateChallenge(challengeToUpdate.copy(name = "Francisco"))
-//
-//        assertThat(scenarios.getCompetition(competitionId).challenges)
-//                .extracting<String> { it.name }
-//                .containsOnly("Francisco")
-//    }
+
+    @Test
+    fun `Adding Challenges, one of which is invalid, should not add any Challenge`() {
+        val challengeToBeCreated = ChallengeToAdd(
+            name = "Picasso",
+            points = 3,
+            description = "Paint a mustache on a sleeping victim without getting caught"
+        )
+        val anotherChallengeToBeCreated =
+            ChallengeToAdd(name = "SomethingElse", points = 5, description = "Something...")
+        val invalidChallengeToBeCreated =
+            ChallengeToAdd(name = "Invalid Challenge", points = 0, description = "Invalid because points cannot be 0")
+
+        val competition = CreateCompetition(
+            name = "InvalidChallengesComp",
+            startDate = LocalDate.of(2018, 3, 16),
+            endDate = LocalDate.of(2018, 3, 26)
+        )
+        val competitionId = scenarios.createNewCompetition(competition)
+
+        assertTrue(scenarios.getCompetition(competitionId).challenges.isEmpty())
+
+        val challengesToBeAdded = listOf(challengeToBeCreated, invalidChallengeToBeCreated, anotherChallengeToBeCreated)
+
+        assertFailsWith(DomainValidationRuntimeException::class) {
+            scenarios.addChallenges(
+                competitionId,
+                challengesToBeAdded
+            )
+        }
+
+        assertContains(scenarios.getCompetition(competitionId).challenges.map { it.name }, challengeToBeCreated.name)
+        //TODO: this kind of transactionality probably doesn't make a lot of sense, or it should be completely controlled by the Competition aggregate itself.
+    }
+
+    @Test
+    fun `Challenges should get copied when hosting a Competition again`() {
+        val challengeToBeCreated = ChallengeToAdd(
+            name = "Picasso",
+            points = 3,
+            description = "Paint a mustache on a sleeping victim without getting caught"
+        )
+
+        val competition = CreateCompetition(
+            name = "DummyCompetition",
+            startDate = LocalDate.of(2018, 3, 16),
+            endDate = LocalDate.of(2018, 3, 26)
+        )
+        val competitionId = scenarios.createNewCompetition(competition, challengeToBeCreated)
+        val originalChallengeId = scenarios.getCompetition(competitionId).challenges.first().id
+
+        val hostedAgainCompetition = CreateCompetition(
+            name = "DummyCompetitionThe2nd",
+            startDate = LocalDate.of(2018, 3, 16),
+            endDate = LocalDate.of(2018, 3, 26)
+        )
+        val hostedAgainCompetitionId = scenarios.createNewCompetition(hostedAgainCompetition, challengeToBeCreated)
+
+        assertNotEquals(originalChallengeId, scenarios.getCompetition(hostedAgainCompetitionId).challenges.first().id)
+    }
+
+    @Test
+    @Ignore
+    fun `Challenges can get updated when a Competition is not yet started`() {
+        val picassoChallenge = ChallengeToAdd(
+            name = "Picasso",
+            points = 3,
+            description = "Paint a mustache on a sleeping victim without getting caught"
+        )
+
+        val competition = CreateCompetition(
+            name = "CompetitionWChallenges",
+            startDate = LocalDate.of(2018, 3, 16),
+            endDate = LocalDate.of(2018, 3, 26)
+        )
+        val competitionId = scenarios.createNewCompetition(competition, picassoChallenge)
+
+        val challengeToUpdate =
+            scenarios.getCompetition(competitionId).challenges.find { it.name == picassoChallenge.name }
+                ?: fail("Expected a challenge with name ${picassoChallenge.name}")
+
+        //TODO: updating a challenge should always require a competition.
+        scenarios.updateChallenge(challengeToUpdate.copy(name = "Francisco"))
+
+        assertContains(
+            scenarios.getCompetition(competitionId).challenges.map { it.name },
+            "Francisco"
+        )
+    }
+
+    @Test
+    @Ignore
+    fun `Challenges can get updated when a Competition was unstarted`() {
+        val challengeToBeCreated = ChallengeToAdd(name = "Picasso", points = 3, description = "Paint a mustache on a sleeping victim without getting caught")
+
+        val competition = CreateCompetition(name = "UnstartedCompWChallenges",
+                startDate = LocalDate.of(2018, 3, 16),
+                endDate = LocalDate.of(2018, 3, 26))
+        val competitionId = scenarios.createNewCompetition(competition, challengeToBeCreated)
+        scenarios.addCompetitor("Snarf", competitionId)
+
+        scenarios.startCompetition(competitionId)
+        scenarios.unstartCompetition(competitionId)
+
+        val challengeToUpdate = scenarios.getCompetition(competitionId).challenges.find { it.name == challengeToBeCreated.name }
+                ?: fail("Expected a challenge with name ${challengeToBeCreated.name}")
+
+        //TODO: updating a challenge should always require a competition.
+        scenarios.updateChallenge(challengeToUpdate.copy(name = "Francisco"))
+
+        assertContains(
+            scenarios.getCompetition(competitionId).challenges.map { it.name },
+            "Francisco"
+        )
+    }
 
 }
 
